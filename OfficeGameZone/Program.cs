7f35cbe101d11e51fe50080d634fb6618b753b31
usamplazo <petrovicgligorijeTSI10B@gmail.com>
2024-08-10 16:09:38 +0200

    project setup

--- OfficeGameZone/Program.cs
+++ OfficeGameZone/Program.cs
@@ -1,15 +1,18 @@
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+
+builder.Host.UseSerilog((context, configuration) => 
+    configuration.ReadFrom.Configuration(context.Configuration));
+
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
